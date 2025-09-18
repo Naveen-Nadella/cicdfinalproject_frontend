855ae3f2d84c6a78c9e57b35db3aa6dff0562082
Naveen-Nadella <naveennadella02@gmail.com>
2025-09-18 19:16:27 +0530

    update2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/khata-frontend/',  // Set base if serving from subdir
   plugins: [react()],
 })
